d3c8fea17289ac46563766f625bc9fa6cfcdeced
Vinay Reddy <vinayreddy91.in@gmail.com>
2017-02-18 16:04:05 -0600

    GET /todos/:id

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Watch new Movie',
+	completed: true
+}, {
+	id: 2,
+	description: 'Study a lot',
+	completed: false
+}, {
+	id: 3,
+	description: 'sleep a lot',
+	completed: true
+}];
 
 app.get('/', function(req,res){
 	res.send('TODO Api Root');
+});
+
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id);
+	var matchedTodo;
+
+	todos.forEach(function(todo){
+		if(todoId === todo.id){
+			matchedTodo = todo;
+		}
+	});
+
+	if(matchedTodo){
+		res.json(matchedTodo);
+	} else{
+		res.status(404).send();
+	}
 });
 
 app.listen(PORT, function(){
